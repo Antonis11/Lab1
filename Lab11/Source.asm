a27328586622653c893c448230859b67046c3ce7
dit20152@office365.uop.gr <dit20152@office365.uop.gr>
2022-08-17 10:45:30 +0300

    Declare variables and symbolic constants

--- Lab11/Source.asm
+++ Lab11/Source.asm
@@ -1,9 +1,11 @@
 title Laboratory 1 : Evaluating Simple Arithmetic Expression
 Include Irvine32.inc
-
+    p EQU 8d
 .data
-
-
+    x SDWORD 16d
+    y SDWORD 154d
+    z DWORD -17d
+    w SDWORD ?
 .code
 main PROC
 
